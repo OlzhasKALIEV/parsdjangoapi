444c514c452b9e31ccf5903de8cad8dc7d4f8ed5
OlzhasKALIEV <130698888+OlzhasKALIEV@users.noreply.github.com>
2023-09-30 01:02:08 +0600

    commit

--- parsapi/urls.py
+++ parsapi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from . import views
+from .views import PatrioticMusicVS, PatrioticMusicRUD, PatrioticMusicDetailView, PatrioticMusicComposerGenre
 
 urlpatterns = [
-    path("patriotic-music/", views.PatrioticMusicVS.as_view(), name="patriotic-music"),
+    path("patriotic-music/", PatrioticMusicVS.as_view(), name="patriotic-music"),
+    path("patriotic-music/<int:pk>/", PatrioticMusicRUD.as_view(), name="patriotic-music-pk"),
+    path("patriotic-music", PatrioticMusicDetailView.as_view(), name="patriotic-music-id"),
+    path("patriotic-music/music", PatrioticMusicComposerGenre.as_view(), name="music"),
 ]
-
-
--- parsapi/views.py
+++ parsapi/views.py
@@ -1,7 +1,8 @@
-
+from django.http import JsonResponse
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from parsapi.models import CatalogPatrioticMusic
 from parsapi.serializers import MuseumFundRFSerializer
@@ -18,7 +19,71 @@ class PatrioticMusicVS(generics.ListCreateAPIView):
     serializer_class = MuseumFundRFSerializer
     pagination_class = LargeResultsSetPagination
 
-    # def list(self, request, *args, **kwargs):
-    #     patriotic_music = CatalogPatrioticMusic.objects.all()
-    #     serializer = MuseumFundRFSerializer(patriotic_music, many=True)
-    #     return Response({"patriotic-music": serializer.data})
+
+class PatrioticMusicRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CatalogPatrioticMusic.objects.all()
+    serializer_class = MuseumFundRFSerializer
+
+
+class PatrioticMusicDetailView(APIView):
+
+    def get(self, request):
+        id = request.GET.get('id')
+        try:
+            music = CatalogPatrioticMusic.objects.get(pk=id)
+            data = {
+                'id': music.id,
+                'fullname': music.fullname,
+                'composer': music.composer,
+                'genre': music.genre,
+                'theme': music.theme,
+                'creationyear': music.creationyear,
+            }
+            return Response({"results": data})
+        except CatalogPatrioticMusic.DoesNotExist:
+            return JsonResponse({'error': 'Music not found'}, status=404)
+
+    def put(self, request):
+        id = request.GET.get('id')
+        try:
+            music = CatalogPatrioticMusic.objects.get(pk=id)
+            # Обновление полей модели
+            music.composer = request.data.get('composer')
+            music.fullname = request.data.get('fullname')
+            music.save()
+            return Response({'success': 'Music updated'})
+        except CatalogPatrioticMusic.DoesNotExist:
+            return Response({'error': 'Music not found'}, status=404)
+
+    def delete(self, request):
+        id = request.GET.get('id')
+        try:
+            music = CatalogPatrioticMusic.objects.get(pk=id)
+            music.delete()
+            return Response({'success': 'Music deleted'})
+        except CatalogPatrioticMusic.DoesNotExist:
+            return Response({'error': 'Music not found'}, status=404)
+
+
+class PatrioticMusicComposerGenre(APIView):
+    serializer_class = MuseumFundRFSerializer
+
+    def get(self, request):
+        composer = request.GET.get('composer')
+        genre = request.GET.get('genre')
+
+        try:
+            if composer:
+                music = CatalogPatrioticMusic.objects.filter(composer=composer)
+            elif genre:
+                music = CatalogPatrioticMusic.objects.filter(genre=genre)
+            else:
+                return Response({'error': 'No search parameters provided'}, status=400)
+
+            if len(music) == 0:
+                return Response({"results": "Music not found"})
+
+            serializers = MuseumFundRFSerializer(music, many=True)
+            return Response({"results": serializers.data})
+        except CatalogPatrioticMusic.DoesNotExist:
+            return Response({'error': 'Music not found'}, status=404)
